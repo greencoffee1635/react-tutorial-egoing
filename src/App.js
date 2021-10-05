034be5df660dc0b9d7865aeff53ea725a0a13518
Jinyeong Kwak <166354@gmail.com>
2021-10-05 13:35:43 +0900

    컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-function ArticleTag(){
+function Article() {
   return (
     <article>
       <h2>안녕</h2>  
@@ -8,17 +8,27 @@ function ArticleTag(){
     </article> 
   )
 }
+function Header() {
+  return (
+    <header><h1><a href="index.html">WEB</a></h1></header>
+  )
+}
+function Nav() {
+  return(
+    <nav>
+      <ol>
+        <li><a href="1.html">html</a></li>
+        <li><a href="2.html">css</a></li>
+      </ol>
+    </nav>
+  );
+}
 function App() {
   return (
     <div>
-      <header><h1><a href="index.html">WEB</a></h1></header>
-      <nav>
-        <ol>
-          <li><a href="1.html">html</a></li>
-          <li><a href="2.html">css</a></li>
-        </ol>
-      </nav>
-      <ArticleTag></ArticleTag>  
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>  
     </div>
   );
 }
