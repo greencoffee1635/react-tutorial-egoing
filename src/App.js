d6b5541a3684f859d7dc648b57803b2d3856a993
Jinyeong Kwak <166354@gmail.com>
2021-10-07 11:06:17 +0900

    네비게이션을 위한 이벤트 기능 구현

--- src/App.js
+++ src/App.js
@@ -10,11 +10,15 @@ function Article(props){
 }
 function Nav(props){
   var lis = [];
+  function aHandler(ev){
+    ev.preventDefault();
+    props.onSelect(ev.target.dataset.id);
+  }
   for(var i = 0; i < props.src.length; i++){
       var item = props.src[i];
       lis.push(
         <li key={item.id}>          
-            <a href={item.id+".html"}>
+            <a href={item.id+".html"} data-id={item.id} onClick={aHandler}>
               {item.title}
             </a>
         </li>
@@ -36,19 +40,24 @@ function Header(props){
   return (
     <header><h1><a href="index.html" onClick={aHandler}>{props.title}</a></h1></header>
   )
-}
+} 
 function App() {
   var topics = [
     {id:1, title:'html', body:'html is ..'},
     {id:2, title:'css', body:'css is ..'}
   ];
-  function selectHandler(){
-    alert('selected!');
+  function selectHandler(id){
+    if(id===undefined){
+      // Welcome 나온다.
+    } else {
+      // 선택한 게시글이 출력된다. 
+    }
+    alert('selected!'+id);
   }
   return (
     <div>
       <Header title="html" onSelect={selectHandler}></Header>
-      <Nav src={topics}></Nav>
+      <Nav src={topics} onSelect={selectHandler}></Nav>
       <Article title="HTML" body="HTML is ..."></Article>  
     </div>
   );
