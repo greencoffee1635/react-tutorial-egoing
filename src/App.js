ab309734069fc3198ff631a146817a646d973d60
Jinyeong Kwak <166354@gmail.com>
2021-10-14 14:32:35 +0900

    create 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import {useState} from 'react';
 import { Article } from './components/Article';
 import { Nav } from './components/Nav';
 import { Header } from './components/Header';
 import { Create } from './components/Create';
 import { Control } from './components/Control';
 import { Update } from './components/Update';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Route, useHistory } from 'react-router-dom';
 
 function App() {
-  
+  var history = useHistory();
   var [id, setId] = useState(2);
   var [mode,setMode] = useState('WELCOME');
   var [nextId,setNextId] = useState(3);
@@ -37,17 +37,7 @@ function App() {
     }
     articleComp = <Article title={title} body={body}></Article>
   } else if(mode === 'CREATE') {
-    function createHandler(_title,_body){
-      // topics.push({title:_title, body:_body});
-      // setTopics(topics);
-      var newTopics = [...topics];
-      newTopics.push({id:nextId, title:_title, body:_body});
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
-    }
-    articleComp = <Create onCreate={createHandler}></Create>
+
   } else if(mode === 'UPDATE'){
     function updateHandler(_title,_body){
       var newTopics = [];
@@ -90,19 +80,28 @@ function App() {
       setMode(_mode);
     }
   }
+  function createHandler(_title,_body){
+    // topics.push({title:_title, body:_body});
+    // setTopics(topics);
+    var newTopics = [...topics];
+    newTopics.push({id:nextId, title:_title, body:_body});
+    setTopics(newTopics);
+    history.push('/read/'+nextId);
+    setNextId(nextId+1);
+  }
   return (
     <div>
-      <Router>
-        <Header title="html"></Header>
-        <Nav src={topics} onSelect={selectHandler}></Nav>
-        <Route exact path="/">
-          <Article title="Welcome" body="Hello, WEB"></Article>
-        </Route>
-        <Route path="/create">Create</Route>
-        <Route path="/read/:id">Read</Route>
-        <Route path="/update/:id">Update</Route>
-        <Control onChangeMode={changeHandler}></Control>
-      </Router>
+      <Header title="html"></Header>
+      <Nav src={topics} onSelect={selectHandler}></Nav>
+      <Route exact path="/">
+        <Article title="Welcome" body="Hello, WEB"></Article>
+      </Route>
+      <Route path="/create">
+        <Create onCreate={createHandler}></Create>
+      </Route>
+      <Route path="/read/:id">Read</Route>
+      <Route path="/update/:id">Update</Route>
+      <Control onChangeMode={changeHandler}></Control>
     </div>
   );
 }
