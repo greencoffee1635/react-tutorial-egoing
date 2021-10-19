9ad3f9d7f3bdc3cd9db8a31c15521bdc1a45600f
Jinyeong Kwak <166354@gmail.com>
2021-10-19 11:34:08 +0900

    control의 update와 delete를 read/:id일 때만 출력 되도록 개선

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,17 +1,19 @@
-import {Link} from 'react-router-dom';
+import {Link, Route} from 'react-router-dom';
 export function Control(props) {
   return (
     <ul>
       <li><Link to="/create">Create</Link></li>
-      <li><Link to="/update/1">update</Link></li>
-      <li>
-        <form onSubmit={function (ev) {
-          ev.preventDefault();
-          props.onChangeMode('DELETE');
-        }}>
-          <input type="submit" value="delete"></input>
-        </form>
-      </li>
+      <Route path="/read/:id">
+        <li><Link to="/update/1">update</Link></li>
+        <li>
+          <form onSubmit={function (ev) {
+            ev.preventDefault();
+            props.onChangeMode('DELETE');
+          }}>
+            <input type="submit" value="delete"></input>
+          </form>
+        </li>
+      </Route>
     </ul>
   );
 }
